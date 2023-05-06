3500599bc5dab75978a597acd323d512bb7278a2
Jack Yeo <superjackxh@gmail.com>
2023-05-06 14:28:19 +0800

    f

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from bottle import route, run, template
+import os
 
 
 @route('/hello/<name>')
@@ -6,4 +7,7 @@ def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
 
-run()
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)

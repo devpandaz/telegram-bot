8347ff50d94ffc93845337f0c461a8dd1e021c4d
Jack Yeo <superjackxh@gmail.com>
2023-05-06 14:33:17 +0800

    f

--- api/index.py
+++ api/index.py
@@ -1,13 +1,11 @@
-from bottle import route, run, template
-import os
+from http.server import BaseHTTPRequestHandler
 
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+class handler(BaseHTTPRequestHandler):
 
-
-if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    run(host='localhost', port=8080, debug=True)
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return

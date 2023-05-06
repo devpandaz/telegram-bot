34300dee23311deca41b697cb0630836ea9b4a93
Jack Yeo <superjackxh@gmail.com>
2023-05-06 16:43:15 +0800

    try

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ Very simple HTTP server in python for logging requests
 Usage::
     ./server.py [<port>]
 """
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import json
 from dotenv import load_dotenv
@@ -53,23 +53,24 @@ class handler(BaseHTTPRequestHandler):
                       })
 
 
-# def run(server_class=HTTPServer, handler_class=S, port=8080):
-#     logging.basicConfig(level=logging.INFO)
-#     server_address = ('', port)
-#     httpd = server_class(server_address, handler_class)
-#     logging.info('Starting httpd...\n')
-#     try:
-#         httpd.serve_forever()
-#     except KeyboardInterrupt:
-#         pass
-#     httpd.server_close()
-#     logging.info('Stopping httpd...\n')
-#
-#
-# if __name__ == '__main__':
-#     from sys import argv
-#
-#     if len(argv) == 2:
-#         run(port=int(argv[1]))
-#     else:
-#         run()
+if os.getenv('APP_LOCATION') == 'vercel':
+
+    def run(server_class=HTTPServer, handler_class=handler, port=8080):
+        logging.basicConfig(level=logging.INFO)
+        server_address = ('', port)
+        httpd = server_class(server_address, handler_class)
+        logging.info('Starting httpd...\n')
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        httpd.server_close()
+        logging.info('Stopping httpd...\n')
+
+    if __name__ == '__main__':
+        from sys import argv
+
+        if len(argv) == 2:
+            run(port=int(argv[1]))
+        else:
+            run()

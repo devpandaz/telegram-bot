56bf9ab5ffe077a54dbdec3291c8488110e592ca
Jack Yeo <superjackxh@gmail.com>
2023-05-06 16:35:41 +0800

    update

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ Very simple HTTP server in python for logging requests
 Usage::
     ./server.py [<port>]
 """
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler
 import logging
 import json
 from dotenv import load_dotenv
@@ -15,8 +15,6 @@ import requests
 load_dotenv()
 
 BOT_URL = f"https://api.telegram.org/bot{os.getenv('BOT_TOKEN')}"
-
-print(BOT_URL)
 
 
 class handler(BaseHTTPRequestHandler):
@@ -55,23 +53,23 @@ class handler(BaseHTTPRequestHandler):
                       })
 
 
-def run(server_class=HTTPServer, handler_class=S, port=8080):
-    logging.basicConfig(level=logging.INFO)
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    logging.info('Stopping httpd...\n')
-
-
-if __name__ == '__main__':
-    from sys import argv
-
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+# def run(server_class=HTTPServer, handler_class=S, port=8080):
+#     logging.basicConfig(level=logging.INFO)
+#     server_address = ('', port)
+#     httpd = server_class(server_address, handler_class)
+#     logging.info('Starting httpd...\n')
+#     try:
+#         httpd.serve_forever()
+#     except KeyboardInterrupt:
+#         pass
+#     httpd.server_close()
+#     logging.info('Stopping httpd...\n')
+#
+#
+# if __name__ == '__main__':
+#     from sys import argv
+#
+#     if len(argv) == 2:
+#         run(port=int(argv[1]))
+#     else:
+#         run()
